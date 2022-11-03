55a92f48d701b019469e7d013071e8232b701d5b
Micah Innerarity <micah@Micahs-MacBook-Air.local>
2022-11-03 18:06:07 -0400

    Added MyPlaces Model

--- src/app/models/my-places.ts
+++ src/app/models/my-places.ts
@@ -1,2 +1,27 @@
 export class MyPlaces {
+    myPlacesId: number;
+    visited: boolean;
+    userId: number;
+    createdOn: string;
+    googlePlacesId: string;
+    placesImages: string;
+    placeName: string;
+    city: string;
+    state: string;
+    starRating: string;
+
+    constructor(createdOn: string, googlePlacesId: string,  myPlacesId?: number, visited?: boolean, userId?: number, placesImages?: string, placeName?: string, city?: string, state?: string, starRating?: string){
+        this.myPlacesId = myPlacesId;
+        this.visited = visited;
+        this.userId = userId;
+        this.createdOn = createdOn;
+        this.googlePlacesId = googlePlacesId;
+        this.placesImages = placesImages;
+        this.placeName = placeName;
+        this.city = city;
+        this.state = state;
+        this.starRating = starRating;
+
+    }
 }
+
