391427ce35b6f04c74c126db67bedb451fbc2f87
Hope Ross <hope.ross@daveramsey.com>
2022-11-07 15:19:52 -0600

    results from geolocation experiment

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Position } from '@capacitor/geolocation';
+import { GeolocationService } from 'src/app/services/geolocation.service';
 
 
 @Component({
@@ -7,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  position: Position = null;
 
-  constructor() { }
+  constructor(private geoService: GeolocationService) { }
 
   ngOnInit() {
   }
 
+  getGPS() {
+    this.geoService.getCurrentPosition().subscribe(result => {
+
+        this.position = result;
+        console.log(this.position.coords.latitude);
+        console.log(this.position.coords.longitude)
+    });
+  }
 }
