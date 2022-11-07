04a246d607ad600eb85adf305536eb1ba6b5de42
Amanda Tutalo <amanda.tutalo@daveramsey.com>
2022-11-07 16:24:02 -0600

    Added Google Places Model

--- src/app/models/google-places.ts
+++ src/app/models/google-places.ts
@@ -1,2 +1,38 @@
 export class GooglePlaces {
+  googlePlaceId: string;
+  placePhotos: string;
+  placeType: string;
+  placeName: string;
+  placeAddress: string;
+  placeHours: string;
+  placeWebSite: string;
+  placePhoneNumber: string;
+  placeRatings: string;
+  placePriceLevel: string;
+
+  constructor(
+  googlePlaceId: string,
+  placePhotos: string,
+  placeType: string,
+  placeName: string,
+  placeAddress: string,
+  placeHours: string,
+  placeWebSite: string,
+  placePhoneNumber: string,
+  placeRatings: string,
+  placePriceLevel: string,
+  ) {
+    this.googlePlaceId = googlePlaceId;
+    this.placePhotos = placePhotos;
+    this.placeType = placeType;
+    this.placeName = placeName;
+    this.placeAddress = placeAddress;
+    this.placeHours = placeHours;
+    this.placeWebSite = placeWebSite;
+    this.placePhoneNumber = placePhoneNumber;
+    this.placeRatings = placeRatings;
+    this.placePriceLevel = placePriceLevel;
+  }
 }
+
+
--- src/app/models/my-places.ts
+++ src/app/models/my-places.ts
@@ -10,7 +10,7 @@ export class MyPlaces {
     state: string;
     starRating: string;
 
-    constructor(createdOn: string, googlePlacesId: string,  myPlacesId?: number, visited?: boolean, userId?: number, placesImages?: string, placeName?: string, city?: string, state?: string, starRating?: string){
+    constructor(createdOn: string, googlePlacesId: string, myPlacesId?: number, visited?: boolean, userId?: number, placesImages?: string, placeName?: string, city?: string, state?: string, starRating?: string){
         this.myPlacesId = myPlacesId;
         this.visited = visited;
         this.userId = userId;
