5fd3ac2c84a027c5c537e7cc4ee58fd43e58e8a2
atutalo <100239540+atutalo@users.noreply.github.com>
2022-11-07 21:16:21 -0600

    Update google-places.ts

--- src/app/models/google-places.ts
+++ src/app/models/google-places.ts
@@ -1,37 +1,28 @@
 export class GooglePlaces {
   googlePlaceId: string;
-  placePhotos: string;
-  placeType: string;
-  placeName: string;
-  placeAddress: string;
-  placeHours: string;
-  placeWebSite: string;
-  placePhoneNumber: string;
-  placeRatings: string;
-  placePriceLevel: string;
+  name: string;
+  type: string;
+  address: string;
+  photo: string;
+  website: string;
+  editorialSummary: string;
 
   constructor(
   googlePlaceId: string,
-  placePhotos: string,
-  placeType: string,
-  placeName: string,
-  placeAddress: string,
-  placeHours: string,
-  placeWebSite: string,
-  placePhoneNumber: string,
-  placeRatings: string,
-  placePriceLevel: string,
+  name: string,
+  type: string,
+  address: string,
+  photo: string,
+  webSite: string,
+  editorialSummary: string,
   ) {
     this.googlePlaceId = googlePlaceId;
-    this.placePhotos = placePhotos;
-    this.placeType = placeType;
-    this.placeName = placeName;
-    this.placeAddress = placeAddress;
-    this.placeHours = placeHours;
-    this.placeWebSite = placeWebSite;
-    this.placePhoneNumber = placePhoneNumber;
-    this.placeRatings = placeRatings;
-    this.placePriceLevel = placePriceLevel;
+    this.name = name;
+    this.type = type;
+    this.address = address;
+    this.photo = photo;
+    this.webSite = webSite;
+    this.editorialSummary = editorialSummary;
   }
 }
 
