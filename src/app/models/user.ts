f21f8279ae409ae76237a759238cf92bdbc20680
Amanda Tutalo <amanda.tutalo@daveramsey.com>
2022-11-08 20:20:33 -0600

    newPush

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,9 +1,24 @@
-export class User {
-  username?: string;
-  password?: string;
+import { MyPlace } from "./my-place";
 
-  constructor(username?: string, password?: string) {
+export class User {
+  userId?: number;
+  username?: string;
+  email?: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+  location?: string;
+
+  myPlaces?: Array<MyPlace>;
+
+  constructor(userId?: number, username?: string, email?: string, password?: string, firstName?: string, lastName?: string, location?: string, myPlaces?: Array<MyPlace>) {
+    this.userId = userId;
     this.username = username;
+    this.email = email;
     this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.location = location;
+    this.myPlaces = myPlaces;
   }
 }
