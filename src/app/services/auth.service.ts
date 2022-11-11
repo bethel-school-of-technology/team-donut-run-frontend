af930b4b792485967f47e9a5eebac96a465810a3
Amanda Tutalo <amanda.tutalo@daveramsey.com>
2022-11-10 21:05:38 -0600

    auth sign up page

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,28 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-
+import { Observable } from 'rxjs';
 import { User } from '../models/user';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   // Use for AuthService methods: signup, sign in
-  authBaseUrl: string = 'http://localhost:5000/api/auth/';
+  authBaseUrl: string = 'http://localhost:5000/api/auth';
   tokenKey: string = 'myUserToken';
 
   constructor(private http: HttpClient) {}
 
-  signup(newUser: User): Observable<any> {
+  signUp(newUser: User): Observable<any> {
+    console.log(newUser);
     return this.http.post(`${this.authBaseUrl}/signup`, newUser);
   }
 
-  signin(username: string, password: string): Observable<any> {
+  signIn(username: string, password: string): Observable<any> {
     let queryParams = new HttpParams();
+    console.log(queryParams);
     queryParams = queryParams.append('username', username);
     queryParams = queryParams.append('password', password);
     return this.http
