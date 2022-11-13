eeb6d485d17bfd5ba80f4475fe19aa7d3b566a4c
Kendall McHenry <kendallexplores@gmail.com>
2022-11-13 14:51:52 -0600

    Fixing something

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarModule } from './components/navbar/navbar.module';
+// import { GooglePlaceModule } from 'google.maps';
 
 @NgModule({
   declarations: [AppComponent],
