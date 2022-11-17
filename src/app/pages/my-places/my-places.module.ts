555009e39b672826abdc3a8decaf7c99049129d2
atutalo <100239540+atutalo@users.noreply.github.com>
2022-11-16 22:18:04 -0600

    Update my-places.module.ts

--- src/app/pages/my-places/my-places.module.ts
+++ src/app/pages/my-places/my-places.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SwiperModule } from 'swiper/angular';
 
 import { IonicModule } from '@ionic/angular';
 
