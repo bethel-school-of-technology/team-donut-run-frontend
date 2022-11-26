a0a0519c874ad03ebcb23f45e39fb75c5ec79149
Amanda Tutalo <amanda.tutalo@daveramsey.com>
2022-11-26 16:20:19 -0600

    fixedSwiperMyPlaces

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SwiperModule } from 'swiper/angular';
-import SwiperCore, {
-  Autoplay,
-  Keyboard,
-  Pagination,
-  Scrollbar,
-  Zoom,
-} from 'swiper';
 import { IonicModule } from '@ionic/angular';
 import { SearchPageRoutingModule } from './search-routing.module';
 import { SearchPage } from './search.page';
 import { NavbarModule } from 'src/app/components/navbar/navbar.module';
 import { FooterModule } from 'src/app/components/footer/navbar.module copy';
-
-SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
 
 @NgModule({
   imports: [
@@ -23,7 +13,6 @@ SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
     FormsModule,
     IonicModule,
     SearchPageRoutingModule,
-    SwiperModule,
     NavbarModule,
     FooterModule,
     
