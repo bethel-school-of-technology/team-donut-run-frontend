a53e2ed471ff35098f90ef4ed36a3da8cc8f9f82
Kendall McHenry <kendallexplores@gmail.com>
2022-11-28 21:19:35 -0600

    Fixing useAPI back to false

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -18,7 +18,7 @@ declare var google;
 export class SearchPage implements OnInit {
   // To use to easily switch between mock and API data
   // TRUE = using Google Data (so, use FALSE most of the time)
-  useAPI: boolean = true;
+  useAPI: boolean = false;
 
   // To get current geolocation
   position: Position = null;
